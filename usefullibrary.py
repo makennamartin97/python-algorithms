be49b2816d08e70f7733fab255939f8c36ec2fb5
makennamartin97 <makennamartin97@yahoo.com>
2020-10-11 21:54:00 -0400

    hacker hero: useful library pt 1

--- usefullibrary.py
+++ usefullibrary.py
@@ -2,15 +2,46 @@ import random
 
 class UsefulLib:
 
-    def filterRange(self, list, min, max):
-        for k in range(len(list)-1):
-            if list[k] < min:
-                list.remove(list[k])
-            if list[k] > max:
-                list.remove(list[k])
-            
+    def multiply(self, list, x):
+        l = len(list)
+        for i in range(l):
+            list[i] = list[i] * x
         return list
-l = UsefulLib()
+    
+    def filterRange(self, list, min, max):
+        res = []
 
-print(l.filterRange([1,3,5,7,9], 2,8) )
-print(l.filterRange([1,3,5,7,9], 4,6))
+        for k in list:
+            num = k
+            if num > min and num < max:
+                res.append(k)
+
+            
+        return res
+
+    def shuffle(self, list, x):
+        l = len(list)
+        index = random.randint(0, l)
+        for i in range(x):
+            temp = list[index]
+            list[index] = list[l-index]
+            list[l-index] = temp
+        return list
+    
+    def randomBetween(self,x,y,z):
+        return random.randrange(x,y,z)
+
+
+
+    
+lib = UsefulLib()
+
+# testing
+print(lib.multiply([2,10,5,3], 7) ) # [14, 70, 35, 21]
+print(lib.filterRange([1,3,5,7,9], 2,8) ) # [3, 5, 7]
+print(lib.filterRange([1,3,5,7,9], 4,6) ) # [5]
+#print(lib.shuffle([1,3,5,7,9,11], 3) ) # [1, 3, 9, 7, 5, 11]
+#print(lib.shuffle([1,3,5,7,9,11], 0) ) # [1, 3, 5, 7, 9, 11]
+
+print(lib.randomBetween(100,200,7))
+
